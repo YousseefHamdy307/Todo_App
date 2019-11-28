ba03ee094cea25166bebfcbd5f85c582234a1763
oyuioy <hgjhgj@yahoo.com>
2019-11-28 02:00:00 +0200

    todo task

---
+++ main.js
@@ -0,0 +1,74 @@
+var removeSpan = '<span>Delete</span>';
+var completeSpan = '<span>Done</span>';
+// user clicked on the add button
+document.getElementById('add').addEventListener('click',function() {
+    var value = document.getElementById('item').value;
+    if (value) addItemTodo(value);
+    document.getElementById('item').value = '';
+    document.getElementById('item').focus();
+});
+
+
+
+function removeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    
+    parent.removeChild(item);
+    
+    
+    
+}
+
+
+function completeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    
+    parent.removeChild(item);
+    
+    
+    
+}
+
+// Add New Items to Todo list
+function addItemTodo(text) {
+    var list = document.getElementById('todo');
+    
+    var item = document.createElement('li');
+    item.innerHTML = text;
+    
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    
+    var remove = document.createElement('button');
+
+    remove.classList.add('remove');
+    remove.innerHTML = 'delete';
+    
+    remove.addEventListener('click',removeItem);
+    
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = 'done';
+    
+    complete.addEventListener('click',completeItem);
+    
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    list.insertBefore(item, list.childNodes[0]); 
+    
+}
+
+// Make Enter Key "add" the Task
+
+var input = document.getElementById("item");
+
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("add").click();
+        }
+    });
